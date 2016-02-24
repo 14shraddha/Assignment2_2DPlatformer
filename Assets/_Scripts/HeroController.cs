1befcb461d485da7f536236ed06327c840cedab6
14shraddha <you@example.com>
2016-02-24 15:47:50 -0500

    Added hero Controller

---
+++ Assets/_Scripts/HeroController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroController : MonoBehaviour {
+    //PRIVATE VARIABLES
+    private Animator _animator;
+    private float _move;
+    private float _jump;
+    private bool _facingRight;
+
+
+	// Use this for initialization
+	void Start () {
+        this._animator = gameObject.GetComponent<Animator> ();
+        this._move = 0f;
+        this._jump = 0f;
+        this._facingRight = true;
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this._move = Input.GetAxis("Horizontal");
+        this._jump = Input.GetAxis("Vertical");
+        if (this._move != 0)
+        {
+            if (this._move > 0)
+            {
+                this._facingRight = true;
+            }
+            //CALL WALK SEQUENCE
+            this._animator.SetInteger("AnimState", 1);
+        }
+        else
+        {
+            //SET IDLE STATE
+            this._animator.SetInteger("AnimState", 0);
+        }
+
+        if (this._jump > 0)
+        {
+            //CALL JUMP SEQUENCE
+            this._animator.SetInteger("AnimState", 1);
+        }
+    }
+}
